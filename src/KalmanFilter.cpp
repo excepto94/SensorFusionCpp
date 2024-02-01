952a2680092ead99eb0d16b78d94dcfa27e1b182
Jonas Ekdahl <karstrom.jonas@gmail.com>
2024-02-01 11:00:02 +0100

    .

--- src/KalmanFilter.cpp
+++ src/KalmanFilter.cpp
@@ -3,49 +3,43 @@
 #include <MotionData.hpp>
 #include <iostream>
 #include <Eigen/Dense>
+#include <KalmanFilter.hpp>
 
-struct KalmanFilter {
-    MotionModel motionModel;
-    SensorModel sensorModel;
-    MotionData motionData;
-    MotionData estimatedData;
+KalmanFilter::KalmanFilter(const MotionModel& mm, const SensorModel& sm, const MotionData& md, const MotionData& ed)
+    : motionModel(mm), sensorModel(sm), motionData(md), estimatedData(ed) {
+}
 
-    KalmanFilter(const MotionModel& mm, const SensorModel& sm, const MotionData& md, const MotionData& ed)
-        : motionModel(mm), sensorModel(sm), motionData(md), estimatedData(ed) {
-        }
+void KalmanFilter::estimate(MotionData measurement, Eigen::VectorXd x, Eigen::MatrixXd P, int steps, bool printEstimates) {
 
-    void estimate(MotionData measurement, Eigen::VectorXd x, Eigen::MatrixXd P, int steps, bool printEstimates) {
-
-        for (int i = 0; i < steps; ++i) {
-            // Prediction step
-            x = motionModel.f(x);
-            P = motionModel.F * P * motionModel.F.transpose() + motionModel.Q;
-            // Update step
-            Eigen::MatrixXd H = sensorModel.H(x);
-            Eigen::VectorXd currentMeasurement = sensorModel.getRelevantMeasurements(measurement, i);
-            Eigen::VectorXd y = currentMeasurement - sensorModel.h(x);
-            Eigen::MatrixXd S = H * P * H.transpose() + sensorModel.R;
-            Eigen::MatrixXd K = P * H.transpose() * S.inverse();
-            x = x + K * y;
-            P = (Eigen::MatrixXd::Identity(motionModel.d, motionModel.d) - K * H) * P;
-            estimatedData = fillDataFromCaModel(estimatedData, x, i);
-            if (printEstimates) {
-                std::cout
-                    << estimatedData.px[i] << ", "
-                    << estimatedData.py[i] << ", "
-                    << std::endl;
-                /*
-                std::cout
-                    << estimatedData.px[i] << ", "
-                    << estimatedData.py[i] << ", "
-                    << estimatedData.vx[i] << ", "
-                    << estimatedData.vy[i] << ", "
-                    << estimatedData.ax[i] << ", "
-                    << estimatedData.ay[i] << ", "
-                    << estimatedData.r[i] << ", "
-                    << estimatedData.a[i] << std::endl;
-                    */
-            }
+    for (int i = 0; i < steps; ++i) {
+        // Prediction step
+        x = motionModel.f(x);
+        P = motionModel.F * P * motionModel.F.transpose() + motionModel.Q;
+        // Update step
+        Eigen::MatrixXd H = sensorModel.H(x);
+        Eigen::VectorXd currentMeasurement = sensorModel.getRelevantMeasurements(measurement, i);
+        Eigen::VectorXd y = currentMeasurement - sensorModel.h(x);
+        Eigen::MatrixXd S = H * P * H.transpose() + sensorModel.R;
+        Eigen::MatrixXd K = P * H.transpose() * S.inverse();
+        x = x + K * y;
+        P = (Eigen::MatrixXd::Identity(motionModel.d, motionModel.d) - K * H) * P;
+        estimatedData = fillDataFromCaModel(estimatedData, x, i);
+        if (printEstimates) {
+            std::cout
+                << estimatedData.px[i] << ", "
+                << estimatedData.py[i] << ", "
+                << std::endl;
+            /*
+            std::cout
+                << estimatedData.px[i] << ", "
+                << estimatedData.py[i] << ", "
+                << estimatedData.vx[i] << ", "
+                << estimatedData.vy[i] << ", "
+                << estimatedData.ax[i] << ", "
+                << estimatedData.ay[i] << ", "
+                << estimatedData.r[i] << ", "
+                << estimatedData.a[i] << std::endl;
+                */
         }
     }
-};
+}
--- src/SensorModel.cpp
+++ src/SensorModel.cpp
@@ -1,35 +1,6 @@
 #include <Eigen/Dense>
 #include <MotionData.hpp>
-
-struct SensorModel {
-    int d1;
-    int d2;
-    std::function<Eigen::MatrixXd(Eigen::VectorXd)> H;
-    Eigen::MatrixXd R;
-    std::function<Eigen::VectorXd(Eigen::VectorXd)> h;
-    std::function<Eigen::VectorXd(MotionData, int)> getRelevantMeasurements;
-
-    // Constructor to set dimensions and initialize matrices
-    SensorModel(int dimension1, int dimension2) : d1(dimension1), d2(dimension2), R(dimension2, dimension2) {
-
-        H = [this](Eigen::VectorXd x) {
-            assert(x.size() == this->d2 && "Input size mismatch in function H");
-            Eigen::MatrixXd result;
-            return result;
-        };
-
-        h = [this](Eigen::VectorXd x) {
-            assert(x.size() == this->d2 && "Input size mismatch in function h");
-            Eigen::VectorXd result;
-            return result;
-        };
-
-        getRelevantMeasurements = [this](MotionData measurement, int i) {
-            Eigen::VectorXd result(d2);
-            return result;
-        };
-    }
-};
+#include <SensorModel.hpp>
 
 SensorModel velocitySensorModel(double T, double sigma) {
     int d1 = 6;
